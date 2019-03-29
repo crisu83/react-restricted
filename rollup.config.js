7da16ae2249d076fbd37528755d03a7fdb7f0991
Christoffer Niska <christofferniska@gmail.com>
2019-03-29 18:48:53 +0100

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,22 @@
-import typescript from "rollup-plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default {
   input: "./src/index.tsx",
-  output: {
-    dir: "./dist",
-    format: "cjs"
-  },
-  plugins: [typescript()]
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [typescript(), terser()]
 };
