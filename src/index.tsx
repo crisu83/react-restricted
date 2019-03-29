a297491ad65014e3245ccf47cc3d238940209b94
Christoffer Niska <christofferniska@gmail.com>
2019-03-29 09:41:57 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,4 @@ export {
   RestrictedConsumer
 } from "./RestrictedContext";
 export { default as RestrictedView } from "./RestrictedView";
-export { default as RestrictedRoute } from "./RestrictedRoute";
 export { default as useRestricted } from "./useRestricted";
