7f2840a76a1c6c04a32496df65217a5ea29717ba
Christoffer Niska <christofferniska@gmail.com>
2019-03-29 18:56:17 +0100

    Silence ts-jest warning

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./test/setup.ts"]
+  setupFilesAfterEnv: ["./test/setup.ts"],
+  globals: {
+    "ts-jest": {
+      diagnostics: {
+        ignoreCodes: [151001]
+      }
+    }
+  }
 };
